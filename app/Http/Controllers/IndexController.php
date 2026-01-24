24824c730772bfc17b102d2bc196add21dbc2a19
pavel <studiovt@mail.ru>
2026-01-24 13:15:16 +0300

    Внес изменения в route для возврата поста/постов

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function index(Request $request){
+
+        $category = $request->query('category');
+        if($category == 'all'){
+            $products = Product::with('category')->get()->toResourceCollection();
+        }
+        else {
+            $products = Product::where('category_id', $request->category)->with('category')->get()->toResourceCollection();
+        }
+
+        return response($products, 200);
+    }
 }
